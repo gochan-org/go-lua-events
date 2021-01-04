4b6c2a7436850d6648d2cb4bda5ecb5619b61612
Eggbertx <eggbertx@users.noreply.github.com>
2021-01-04 15:34:03 -0800

    Remove unnecessary userdata parameter

--- plugins/eventstest.lua
+++ plugins/eventstest.lua
@@ -1,6 +1,6 @@
 print("Testing post-received event")
 
-addListener("post-received", function(post, ud)
+addListener("post-received", function(post)
 	print("Receiving post:")
 	for key,value in pairs(post) do
 		print(key,"=",value)
